d313496d3e476d1eb071cc0b0c4287b212101781
Hazmi35 <contact@hzmi.xyz>
2021-12-07 19:33:31 +0700

    feat(getRecipe): Read metadata from Dockerfile lines

---
+++ src/getRecipe.js
@@ -0,0 +1,10 @@
+import { getValue } from "./util/getValue.js";
+
+export function getRecipe(lines) {
+    return {
+        tags: getValue(lines.at(0)).split(", "),
+        variant: getValue(lines.at(1)),
+        platforms: getValue(lines.at(2)).split(", "),
+        version: getValue(lines.at(3)).split("-").at(0)
+    };
+}
