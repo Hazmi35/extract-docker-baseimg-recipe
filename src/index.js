93d478431587a6781d4fc6a98082ea269ac44e25
Hazmi35 <contact@hzmi.xyz>
2021-12-07 19:46:59 +0700

    feat: Implement all the functions

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 #!/usr/bin/env node
 
+import { getInputArgs } from "./util/getInputArgs.js";
+import { getDockerfile } from "./util/getDockerfile.js";
+import { getRecipe } from "./getRecipe.js";
+import { seperateSemver } from "./util/seperateSemver.js";
+import { applyVariant } from "./util/applyVariant.js";
+
+const { path, key } = getInputArgs();
+const { lines } = getDockerfile(path);
+const { variant, tags: originalTags, version, platforms } = getRecipe(lines);
+
+const versions = seperateSemver(version);
+const tags = [
+    ...versions.map(v => applyVariant(v, variant)),
+    ...originalTags.map(t => applyVariant(t, variant))
+];
+
+const recipe = { tags, variant, platforms, version };
+
+if (key) console.info(recipe[key]);
+else console.info(recipe);
