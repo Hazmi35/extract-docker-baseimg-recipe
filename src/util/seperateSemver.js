8632dec03a644ecb1d23ade06407d012656bb89e
Hazmi35 <contact@hzmi.xyz>
2021-12-09 21:23:47 +0700

    feat(seperateSemver): Better semver seperator function

--- src/util/seperateSemver.js
+++ src/util/seperateSemver.js
@@ -1,11 +1,5 @@
 // A function that seperate a semver. For example: 14.18.1 -> ['14', '14.18', '14.18.1']
 export function seperateSemver(version) {
-    const versions = [];
-    let i = 0;
-    const semver = version.split(".");
-    while (i !== semver.length) {
-        versions.push(semver.slice(0, i + 1).join("."));
-        i++;
-    }
-    return versions;
+    const semverArray = version.split(".");
+    return semverArray.map((_, i) => semverArray.slice(0, semverArray.length - i).join("."));
 }
