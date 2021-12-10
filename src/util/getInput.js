aca7943ecbf1a22d40d1ba595b58ef52b991fe10
Hazmi35 <contact@hzmi.xyz>
2021-12-11 02:38:25 +0700

    feat: Flags to toggle whether to apply variant to tags or not

---
+++ src/util/getInput.js
@@ -0,0 +1,3 @@
+export function getInput(input, flags) {
+    return { path: input.at(0), key: input.at(1), flags };
+}
--- src/util/getInputArgs.js
+++
@@ -1,3 +0,0 @@
-export function getInputArgs(input) {
-    return { path: input.at(0), key: input.at(1) };
-}
