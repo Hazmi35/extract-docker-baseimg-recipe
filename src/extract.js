0213114a98b140cc918e8afbebdc1549f4e2eea1
Hazmi35 <contact@hzmi.xyz>
2021-12-11 02:31:21 +0700

    feat(extract): Add applyVariant option, defaulting to true

--- src/extract.js
+++ src/extract.js
@@ -3,7 +3,7 @@ import { getRecipe } from "./util/getRecipe.js";
 import { seperateSemver } from "./util/seperateSemver.js";
 import { applyVariant } from "./util/applyVariant.js";
 
-export function extract(path) {
+export function extract(path, options = { applyVariant: true }) {
     const { lines } = getDockerfile(path);
     const { variant, tags: originalTags, version, platforms } = getRecipe(lines);
 
@@ -11,7 +11,7 @@ export function extract(path) {
     const tags = [
         ...versions,
         ...originalTags
-    ].map(t => applyVariant(t, variant));
+    ].map(t => options.applyVariant ? applyVariant(t, variant) : t);
 
     return { tags, variant, platforms, version };
 }
